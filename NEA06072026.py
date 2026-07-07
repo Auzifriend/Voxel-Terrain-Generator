da9ecf40d44b6b50eee5e10747068807be17c664
Auzifriend <awh070209@gmail.com>
2026-07-07 17:39:59 +0100

    Add files via upload

---
+++ NEA06072026.py
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+world = open("World.txt", "r")
+
+def fileRead(length):
+    for line in world:
+        for i in range(0, length, 5):
+            print(line[i:i+5])
+
+
+fileRead(25)
