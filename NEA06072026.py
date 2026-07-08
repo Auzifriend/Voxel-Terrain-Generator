663a4465243b315e4bd5ec9b44fd86a315cde8c8
Auzifriend <awh070209@gmail.com>
2026-07-08 18:06:31 +0100

    Update NEA06072026.py
    xyz, texture -> ASCII works

--- NEA06072026.py
+++ NEA06072026.py
@@ -1,40 +1,46 @@
+from random import randint
 
+ASCIItable = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
 
+def ASCIIconv(number):
+    digits = ""
+    while number:
+        digits += ASCIItable[number%95]
+        number //= 95
+    return digits
 
+def giveBlock(x,y,z, texture):
+    binX = format(x, '08b')
+    binY = format(y, '08b')
+    binZ = format(z, '08b')
+    binTexture = format(texture, '03b')
 
+    bigBin = "1"+binX+binY+binZ+binTexture
+    bigInt = int(bigBin, 2)
 
+    return ASCIIconv(bigInt)
 
+def getBlocks(file):
+    length = len(file.readline())
+    file.seek(0)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-asd
-
-
-
-
-
-
-
-
-
-world = open("World.txt", "r")
-
-def fileRead(length):
-    for line in world:
+    for line in file:
         for i in range(0, length, 5):
-            print(line[i:i+5])
+            block = line[i:i+5]
+            print(block)
+
+world = open('World.txt', 'r+')
+
+for x in range(10):
+    for y in range(10):
+        for z in range(10):
+            block = giveBlock(randint(0,255),randint(0,255),randint(0,255),randint(0,5))
+            world.write(block)
+        #world.write('\n')
+    #world.write('\n\n')
 
 
-fileRead(25)
+
+
+
+
